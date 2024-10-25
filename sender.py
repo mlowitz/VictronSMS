f7f8b640f6b515296f2faa25ef48734d6fa21aeb
morris <morris.lowitz@gmail.com>
2024-10-24 18:14:40 -0700

    remotve

--- sender.py
+++ sender.py
@@ -4,8 +4,7 @@ from fastapi import FastAPI
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "AC81a2da88f4c9b633f64f8d3c20d999f6"
-auth_token = "ccd593a8fa886e93481f6a44dad2dcf1"
+
 client = Client(account_sid, auth_token)
 
 
